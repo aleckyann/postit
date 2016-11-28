ed46b6e92c8cba82a16203aaa6762ed568cb6f02
aleckyann@gmail.com <aleckyann@gmail.com>
2016-11-28 00:23:04 -0200

    Exemplo v2

--- Exemplo/index.php
+++ Exemplo/index.php
@@ -1,5 +1,5 @@
 
-    <script src="../postit-release.js"></script>
+    <script src="../postit2.js"></script>
 
     Exemplo:<br>
     <small>postit('/', ['name=Aleck Yann Mattos', 'idade=22', 'email=aleckyann@gmail.com', 'sexo=masculino'])</small>
