e6ce324faa056562792e11d30a5c9e0bd514eb7a
aleckyann@gmail.com <aleckyann@gmail.com>
2016-12-02 17:30:46 -0200

    Concertando src

--- Exemplo/index.php
+++ Exemplo/index.php
@@ -8,8 +8,6 @@
 
     <img src="img.jpg" onclick="postit('./', ['name=aleck', 'idade=22', 'email=aleckyann@gmail.com', 'sexo=masculino'])"></img>
     
-    <img src="img.jpg" postit-action="./" postit-inputs="name:aleck, idade:22, email:aleckyann@gmail.com, sexo:masculino" onclick="postit(this)"></img>
-    
     <hr>
 
     <pre>
